cca73c092f37b21acc94030486c1f9658c1f8a1d
n-hirahara <genenao21@gmail.com>
2022-09-11 20:16:24 +0900

    Add: version2

--- backend/app.js
+++ backend/app.js
@@ -35,9 +35,11 @@ app.get("/", (req, res) => {
 //  追加
 app.post("/todo/add", (req, res) => {
   const todo = req.body;
+  console.log(todo, "__body");
+
   try {
     pool.query(
-      "INSERT INTO todos (id, name, description) values(null, ?, ?)",
+      "INSERT INTO todos (id, name, description) VALUES(null, ?, ?)",
       [todo.name, todo.description],
       (error, results, fields) => {
         if (error) throw error;
@@ -55,13 +57,41 @@ app.get("/todo/:id", (req, res) => {
 });
 
 // 課題② 編集
-app.put("/todo/:id", (req, res) => {
-  return res.json({ status: true });
+app.put("/todo", (req, res) => {
+  const todo = req.body;
+  console.log(todo, "__body");
+
+  try {
+    pool.query(
+      "UPDATE todos SET name = ?, description = ? WHERE id = ?",
+      [todo.name, todo.description, todo.id],
+      (error, results, fields) => {
+        if (error) throw error;
+      }
+    );
+    return res.json({ status: true });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 // 課題③ 削除
-app.delete("/todo/:id", (req, res) => {
-  return res.json({ status: true });
+app.delete("/todo", (req, res) => {
+  console.log(req.query, "__query");
+  const todoId = req.query.id;
+
+  try {
+    pool.query(
+      "DELETE FROM todos WHERE id = ?",
+      [todoId],
+      (error, results, fields) => {
+        if (error) throw error;
+      }
+    );
+    return res.json({ status: true });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 //  サーバー起動
--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,9 @@
 const baseUrl = "http://localhost:3000";
 const list = document.querySelector(".todos");
 const addTodo = document.querySelector(".add");
+const editTodo = document.querySelector(".edit");
+
+console.log(editTodo, "todo");
 
 // 追加
 addTodo.addEventListener("submit", async (e) => {
@@ -19,8 +22,30 @@ addTodo.addEventListener("submit", async (e) => {
   return console.log("ok");
 });
 
+// 編集
+editTodo.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  console.log(editTodo.id.value.trim(), "id");
+
+  const values = {
+    id: editTodo.id.value.trim(),
+    name: editTodo.name.value.trim(),
+    description: addTodo.description.value.trim(),
+  };
+
+  const data = await EditTodo(values);
+  console.log(data, "__data");
+
+  if (!data.status) {
+    return console.error(`CreateTodo Error`);
+  }
+
+  // ok
+  return console.log("ok");
+});
+
 const CreateTodo = async (values) => {
-  console.log(values, "values");
+  console.log(values, "__values");
   const res = await axios.post(`${baseUrl}/todo/add`, values);
   return await res.data;
 };
@@ -35,6 +60,8 @@ const GetTodo = async () => {
             <li>
                 <h3>${todo.name}</h3>
                 <span>${todo.description}</span>
+
+                <input type='button' value=${todo.id} onClick="DeleteTodo(event)">削除</input>
             </li>
             `;
       temp += html;
@@ -42,6 +69,34 @@ const GetTodo = async () => {
     list.innerHTML = temp;
   } catch (err) {
     console.error(`[GetTodo Error]`, err);
+  }
+};
+
+// 編集
+const EditTodo = async (values) => {
+  try {
+    //  リクエスト
+    const res = await axios.put(`${baseUrl}/todo`, values);
+    return await res.data;
+
+    //  ハンドリング
+  } catch (err) {
+    console.error(`[DeleteTodo Error]`, err);
+  }
+};
+
+// 削除
+const DeleteTodo = async (event) => {
+  const todoId = event.target.value;
+
+  try {
+    //  リクエスト
+    const res = await axios.delete(`${baseUrl}/todo?id=${todoId}`);
+    console.log(res);
+
+    //  ハンドリング
+  } catch (err) {
+    console.error(`[DeleteTodo Error]`, err);
   }
 };
 
